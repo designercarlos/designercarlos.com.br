d64b46dae127e5e53374bc689f0621800f8508fa
Carlos Ferreira <charliefferreira@gmail.com>
2024-05-03 16:30:23 -0300

    Tamanhos, espaçamentos e comportamento: - Título - Texto de fundo do hero (special-txt) - Serviços - Acordeões
     Changes to be committed:
    	modified:   JS/accordion.js
    	modified:   css/components.css
    	modified:   css/main.css
    	modified:   css/style.css
    	modified:   home.html

--- JS/accordion.js
+++ JS/accordion.js
@@ -3,14 +3,21 @@ const accordions = document.querySelectorAll(".accordion");
 
 // Add the onclick listeners.
 accordions.forEach((targetAccordion) => {
-  targetAccordion.addEventListener("click", () => {
+  targetAccordion.addEventListener("toggle", () => {
+    //If opening the accordion
+    if (targetAccordion.open){
 
-    // Close all the accordions that are not targetAccordion.
-    accordions.forEach((accordion) => {
-      if (accordion !== targetAccordion) {
-        accordion.open = false;
+      // Close all the accordions that are not targetAccordion.
+      accordions.forEach((accordion) => {
+        if (accordion !== targetAccordion) {
+          accordion.open = false;
+        }
+      });
+
+      //If the target accordion appears below half screen
+      if (targetAccordion.getBoundingClientRect().top > window.innerHeight/2){
+        window.scrollBy(0, targetAccordion.getBoundingClientRect().top - 100);// scroll to the target accordion and leave space for header (100)
       }
-    });
-    window.scrollBy(0, targetAccordion.getBoundingClientRect().top - 100);
+    }
   });
 });
